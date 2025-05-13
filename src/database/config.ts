ef24cc69154cd6c4faad8cd89837c73483dabaa3
Sedrowow <sedrickfanderl8@gmail.com>
2025-05-13 12:45:55 +0200

    fix: change database dialect from mysql to sqlite

--- src/database/config.ts
+++ src/database/config.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(process.env.DB_NAME as string, process.env.DB_USER as string, process.env.DB_PASSWORD as string, {
     host: process.env.DB_HOST,
-    dialect: 'mysql', // or 'postgres', 'sqlite', etc.
+    dialect: 'sqlite', // or 'postgres', 'sqlite', etc.
 });
 
 export default sequelize;
